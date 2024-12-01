54e2fa25bc6f9ca9e342b413d72b6224b58aa424
조혜린 <hyer1205@gmail.com>
2024-12-01 19:42:26 +0900

    Refactor(calculator): 커스텀 구분자 처리를 정적 필드로 리팩터링
    커스텀 구분자 지정 문자열인 //와 \n을 정적 필드로 정의
    객체마다 중복 생성하지 않고 공통된 값을 공유하기 위해 정적 필드를 사용

--- javascript-calculator-7/src/Calculator.js
+++ javascript-calculator-7/src/Calculator.js
@@ -1,6 +1,9 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class Calculator {
+  static CUSTOM_DELIMITER_PREFIX = '//';
+  static CUSTOM_DELIMITER_SUFFIX = '\\n';
+
   async run() {
     const input = await Console.readLineAsync(
       '덧셈할 문자열을 입력해 주세요.\n',
@@ -13,9 +16,11 @@ class Calculator {
   sum(input) {
     if (input === '') return 0;
 
-    if (input.startsWith('//')) {
-      const customDelimiter = input.split('\\n')[0].substring(2);
-      const inputNums = input.split('\\n')[1];
+    if (input.startsWith(Calculator.CUSTOM_DELIMITER_PREFIX)) {
+      const customDelimiter = input
+        .split(Calculator.CUSTOM_DELIMITER_SUFFIX)[0]
+        .substring(2);
+      const inputNums = input.split(Calculator.CUSTOM_DELIMITER_SUFFIX)[1];
 
       const escapedDelimiter = this.escapeRegExp(customDelimiter);
       return this.calculate(escapedDelimiter, inputNums);
