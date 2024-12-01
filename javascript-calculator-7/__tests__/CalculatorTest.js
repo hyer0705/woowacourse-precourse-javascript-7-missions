88e408cb28f61999ae2b028f879c5ffd6e4c4236
조혜린 <hyer1205@gmail.com>
2024-12-01 19:03:32 +0900

    Feat(calculator): 사용자의 값을 입력받고 결과를 출력하는 기능 구현
    Calculator 클래스에 run()메서드를 구현하여 값을 입력받고 결과를 출력하는 기능 구현

--- javascript-calculator-7/__tests__/CalculatorTest.js
+++ javascript-calculator-7/__tests__/CalculatorTest.js
@@ -13,7 +13,8 @@ describe('Calculator class', () => {
   test('sum() - 커스텀 구분자 테스트', () => {
     const calculator = new Calculator();
 
-    expect(calculator.sum('//;\n1;2;3')).toBe(6);
+    expect(calculator.sum('//;\\n1;2;3')).toBe(6);
+    expect(calculator.sum('//;\\n1')).toBe(6);
   });
 
   test('sum() - 사용자가 잘못된 값을 입력한 경우 테스트', () => {
--- javascript-calculator-7/src/Calculator.js
+++ javascript-calculator-7/src/Calculator.js
@@ -1,9 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class Calculator {
+  async run() {
+    const input = await Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n',
+    );
+
+    const result = this.sum(input);
+    Console.print(`결과 : ${result}`);
+  }
+
   sum(input) {
     if (input === '') return 0;
 
     if (input.startsWith('//')) {
-      const customDelimiter = input.split('\n')[0].substring(2);
+      const customDelimiter = input.split('\\n')[0].substring(2);
       const inputNums = input.split('\n')[1];
 
       return this.calculate(customDelimiter, inputNums);
