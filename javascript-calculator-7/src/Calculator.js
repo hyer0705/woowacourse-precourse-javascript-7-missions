9e6233497b30ccf4c9d5e40c001fd77983e59b88
조혜린 <hyer1205@gmail.com>
2024-12-01 18:22:32 +0900

    Feat(calculator): 커스텀 구분자를 입력했을 때, 계산되는 기능 구현
    '//'로 시작하면 커스텀 구분자가 정의됐다는 것을 의미하므로 커스텀 구분자를 알아낸 후, calculate() 메서드를 호출하여 계산하도록 구현
    커스텀 구분자를 입력했을 때와 기본 구분자를 입력했을 때 계산되는 행위가 같으므로 calculate() 메서드를 분리하여 리팩토링

--- javascript-calculator-7/src/Calculator.js
+++ javascript-calculator-7/src/Calculator.js
@@ -2,13 +2,24 @@ class Calculator {
   sum(input) {
     if (input === '') return 0;
 
-    const nums = input.split(/,|:/).map(Number);
-    const totalSum = nums.reduce(
-      (previousValue, currentNum) => previousValue + currentNum,
-      0,
-    );
+    if (input.startsWith('//')) {
+      const customDelimiter = input.split('\n')[0].substring(2);
+      const inputNums = input.split('\n')[1];
 
-    return totalSum;
+      return this.calculate(customDelimiter, inputNums);
+    }
+
+    return this.calculate(/,|:/, input);
+  }
+
+  calculate(delimiter, inputNumbers) {
+    return inputNumbers
+      .split(delimiter)
+      .map(Number)
+      .reduce(
+        (previousValue, currentNumbers) => previousValue + currentNumbers,
+        0,
+      );
   }
 }
 
