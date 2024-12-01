c36a2125ea851cce74fb5b174fccabf4b0834de4
조혜린 <hyer1205@gmail.com>
2024-12-01 18:05:14 +0900

    Chore(calculatortest): test 설명 문구 수정
    sum() -> sum() 기본 구분자 테스트로 수정

--- javascript-calculator-7/__tests__/CalculatorTest.js
+++ javascript-calculator-7/__tests__/CalculatorTest.js
@@ -1,7 +1,7 @@
 import Calculator from '../src/Calculator.js';
 
 describe('Calculator class', () => {
-  test('sum()', () => {
+  test('sum() - 기본 구분자 테스트', () => {
     const calculator = new Calculator();
 
     expect(calculator.sum('')).toBe(0);
